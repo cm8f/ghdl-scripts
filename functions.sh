31b26d20bfe244c5d4c2afcc85f4a941bc8fbd2a
cm8f <cm8f@artemis>
2023-10-29 10:44:22 +0100

    ghdlify xpm script

--- functions.sh
+++ functions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -z "$NVC_INSTALL_DEST" ]; then
+if [ -z "$GHDL_INSTALL_DEST" ]; then
   if command -v cygpath &>/dev/null; then
-    export NVC_INSTALL_DEST="$(cygpath -m $HOME)/.nvc/lib"
+    export GHDL_INSTALL_DEST="$(cygpath -m $HOME)/.ghdl/lib"
   else
-    export NVC_INSTALL_DEST=$HOME/.nvc/lib
+    export GHDL_INSTALL_DEST=$HOME/.ghdl/lib
   fi
 fi
 
@@ -16,17 +16,17 @@ _safe () {
   [ $? = 0 ] || exit 1
 }
 
-_nvc () {
+_ghdl () {
   local _work=${WORK:-work}
-  local _dest=$NVC_INSTALL_DEST
-  local _opts="--std=${STD:-1993} --work=$_dest/$_work -L$_dest $GLOBAL_OPTS"
+  local _dest=$GHDL_INSTALL_DEST
+  local _opts="-a --std=${STD:-1993} --work=$_work --workdir=$_dest "
   [ -d $_dest ] || _safe mkdir -p $_dest
-  _safe ${NVC:-nvc} $_opts  $*
+  _safe ${GHDL:-ghdl} $_opts  $*
 }
 
 analyse () {
   local _files=$*
-  _nvc -a $A_OPTS $_files
+  _ghdl $_files
 }
 
 analyse_list () {
@@ -40,7 +40,7 @@ git_wrapper () {
   local _repo=$1
   local _base=$(basename $_repo)
   local _tag=$2
-  local _cache=${CACHE:-$HOME/.cache/nvc}/$_base-$_tag
+  local _cache=${CACHE:-$HOME/.cache/ghdl}/$_base-$_tag
   if [ ! -d $_cache/.git ]; then
     _safe mkdir -p $_cache
     _safe git clone --recursive $_repo -b $_tag $_cache --depth=1
